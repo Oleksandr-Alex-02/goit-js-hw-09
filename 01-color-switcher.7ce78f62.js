f1e1ae0f720c336ed02aa5742e0c12aff587e04c
Oleksandr-Alex-02 <100072859+Oleksandr-Alex-02@users.noreply.github.com>
2022-08-10 14:49:47 +0000

    Deploying to gh-pages from @ Oleksandr-Alex-02/goit-js-hw-09@b2d44a9b9ea9176b07fd795c8360ea6272c1027b 🚀

--- 01-color-switcher.7ce78f62.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.setAttribute("disabled",!0)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled")}));let n=null;
-//# sourceMappingURL=01-color-switcher.7ce78f62.js.map
